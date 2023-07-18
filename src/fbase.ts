bcb3de0546a11b77ad923ffd4faf25e8fb7ec378
Dangchan Kim <kdc5847@naver.com>
2023-07-18 23:08:08 +0900

    firstcommit

--- src/fbase.ts
+++ src/fbase.ts
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
